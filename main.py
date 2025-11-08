f44974c35e0f291408a6f20c1710adf0511ad550
gsfreitas <gfreitas322@gmail.com>
2025-11-08 16:00:32 -0300

    f: Calculo de salario e bonus

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+# print(f"Numero de caracteres: {len(input('Enter your name: '))}")
+
+CONSTANT = 0.15
+
+nome = input("Enter your name: ")
+salary = float(input("Enter your salary: "))
+bonus = float(input("Enter your bonus: "))
+
+print(f"The total salary is: {salary + bonus*CONSTANT}")
