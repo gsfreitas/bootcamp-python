d39d03a11a1ce3e73c5bdca81022cf02b2117873
gsfreitas <gfreitas322@gmail.com>
2026-01-31 23:02:19 -0300

    select heros

--- aula_16/main.py
+++ aula_16/main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlalchemy import text
 
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -11,12 +12,17 @@ engine = create_engine("sqlite:///database.db", echo=True)
 
 SQLModel.metadata.create_all(engine)
 
-hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
-hero_2 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
-hero_3 = Hero(name="Spider-Girl", secret_name="Miral Lagoon", age=16)
+# hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson", age=45)
+# hero_2 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+# hero_3 = Hero(name="Spider-Girl", secret_name="Miral Lagoon", age=16)
 
 with Session(engine) as session:
-    session.add(hero_1)
-    session.add(hero_2)
-    session.add(hero_3)
-    session.commit()
+    # select * from hero
+    statement = text("SELECT * FROM hero")
+    result = session.exec(statement)
+    
+    # fetch all
+    heroes = result.fetchall()
+    
+    for hero in heroes:
+        print(f"Hero: {hero.name} - {hero.secret_name} - {hero.age}")
